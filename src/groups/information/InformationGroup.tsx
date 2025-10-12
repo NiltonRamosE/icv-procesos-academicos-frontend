9917a7890d39b06cf7c5be42d13f2ae8ccea34b0
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-10-12 03:37:23 -0500

    feat: agregar estructura de páginas

--- src/groups/information/InformationGroup.tsx
+++ src/groups/information/InformationGroup.tsx
@@ -3,20 +3,17 @@ import React, { useState } from "react";
 import { config } from "config.ts";
 import { Navbar } from "@/shared/Navbar";
 
-export function Courses() {
+export function InformationGroup() {
 
   console.log(`${config.apiUrl}${config.endpoints.auth.login}`);
 
 
   return (
     <>
-      
-      <main>
-        <Navbar />
-        <section className="grid grid-cols-1 md:grid-cols-2 text-white">
-          Este es la página de cursos
-        </section>
-      </main>
+      <Navbar />
+      <section className="grid grid-cols-1 md:grid-cols-2 text-white">
+          Este es el InformationGroup
+      </section>
     </>
   );
 };
