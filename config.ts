8ea9bf4bd5eb2b266c598c92eed73d3f8e1cee01
JuanJo2305 <juanjitoaguilarv@gmail.com>
2025-10-18 16:51:55 -0500

    feat: añadir pestañas Gestión de Grupos y Catálogo y endpoints

--- config.ts
+++ config.ts
@@ -5,13 +5,31 @@
  * Los endpoints estan divididos por secciones, como auth, users, clientes, productos y blogs.
  **/
 export const config = {
-  apiUrl:"https://icv-procesos-academicos-847846098195.us-central1.run.app",
-  //apiUrl:"http://127.0.0.1:8000",
+  //apiUrl:"https://icv-procesos-academicos-847846098195.us-central1.run.app",
+  apiUrl:"http://127.0.0.1:8000",
   environment:"development",
   endpoints: {
     auth: {
       login: "/api/auth/login",
       logout: "/api/auth/logout",
+    },
+    
+    groups: {
+      getById: "/api/groups/:id",
+      getAnnouncements: "/api/groups/:id/announcements",
+      createAnnouncement: "/api/groups/:id/announcements",
+      getEvaluations: "/api/groups/:id/evaluations",
+      createEvaluation: "/api/groups/:id/evaluations",
+      getGrades: "/api/groups/:id/grades",
+      getParticipants: "/api/groups/:id/participants",
+      getClasses: "/api/groups/:id/classes",
+      createClass: "/api/groups/:id/classes",
+    },
+    
+    courses: {
+      getAll: "/api/courses",
+      getById: "/api/courses/:id",
+      getGroups: "/api/courses/:id/groups",
     },
   },
 };
